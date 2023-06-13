7a1ceb50c43fc11c3f4f20e003e9da05402729f3
Kunal Yadav <yadavkunal780@gmail.com>
2023-06-13 22:24:22 +0530

    middlewares and templating

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,24 @@
 import express from 'express';
+import { servicesVersion } from 'typescript';
 
-console.log(express);
+const server = express();
+
+server.use(express.static("dist"));
+
+server.set("view engine","ejs");
+
+server.use("/",(req,res)=>{
+    // res.send("Hello Kunal");
+    res.render("index",{
+        content: "EJS is <em>cool!</em>",
+    });
+});
+
+// console.log(express);
+
+server.listen("8080","localhost",()=>{
+    console.info(
+        "Express server is listening at http://localhost:8080"
+    );
+
+});
